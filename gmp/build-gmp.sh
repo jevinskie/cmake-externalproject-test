8bf777b677b0409e8c7350d6907bef64d0fb2c12
Jevin Sweval <jevinsweval@gmail.com>
2025-02-01 14:59:33 -0500

    builds single threaded

--- gmp/build-gmp.sh
+++ gmp/build-gmp.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
-mkdir -p $(dirname $1)
-echo "'h', 'i', '\0'" > $1
+set -euxo pipefail
+
+echo '"$@:" ' "$@"
+echo '$@: ' $@
+
+mkdir -p $(dirname "$1")
+echo "'h', 'i', '\0'" > "$1"
+
+rm -f empty.o libempty.a
+cc -c empty.s
+ar rs libempty.a empty.o
+rm -f empty.o
+
+mv libempty.a "$2"
