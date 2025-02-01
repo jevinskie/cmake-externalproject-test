984022f79a47decd7bc07552baec02ccfc85000e
Jevin Sweval <jevinsweval@gmail.com>
2025-02-01 01:00:32 -0500

    wip

---
+++ build-gmp.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo "'h', 'i', '\0'" > $1
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+const char str[] = {
+#include "gmp.h"
+};
+
+int main(void) {
+    puts(str);
+    return 0;
+}
